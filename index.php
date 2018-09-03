2c7a418fffd2956f67502255ec3b6feede9ec96d
Orfac <molodeckij.b.2017@fml31.ru>
2018-09-03 23:44:09 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form>
+
+</form>
+<?php
+for ($i=0; $i < 5; $i++) { 
+    echo  '<input type="radio" name="X" value="$i"> Female<br>';
+}
+echo "My first PHP script!";
+?>
+
+
