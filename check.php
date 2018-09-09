e27ab5e59d4c99977e601130421bcacf4cb6d485
Orfac <molodeckij.b.2017@fml31.ru>
2018-09-09 19:20:09 +0300

    stable version

--- check.php
+++ check.php
@@ -1,4 +1,54 @@
 <?php
-    echo "hello";
-    echo _POST;
+    $start = microtime(true);
+    $x = $_POST["X"];
+    $y = $_POST["Y"];
+    $r = $_POST["R"];
+    $r2 = $r / 2;
+    $resultMessage;
+    $currentTime = date("H:i:s");
 ?>
+<!DOCTYPE HTML>
+<html> 
+<head> 
+    <meta charset='UTF-8'> 
+    <title>Проверка</title>
+    <style type="text/css">
+        
+    </style>
+</head> 
+<body> 
+    <table>
+    <tr>  
+        <td>X</td>
+        <td>Y</td>
+        <td>R</td> 
+        <td>Состояние</td> 
+        <td>Время</td> 
+        <td>Время работы скрипта</td>  
+    </tr>
+<?php
+    if (($x >= 0 && $y >= 0 && $y <= $r && $x <= $r) ||
+        ($x <= 0 && $y <= 0 && ($x * $x + $y * $y) <= ($r2 * $r2)) ||
+        ($x >= 0 && $y <= 0 && ($x - $y) <= $r2 )
+        ){
+            $resultMessage = "Точка попадает в область";
+        }       
+      else{
+        $resultMessage = "Точка не попадает в область";
+      }
+          
+      $time = microtime(true) - $start;
+      echo "
+      <tr>
+        <td>$x</td> 
+        <td>$y</td> 
+        <td>$r</td>            
+        <td>$resultMessage</td> 
+        <td>$currentTime</td> 
+        <td>$time</td> 
+       </tr>
+       ";
+    ?>
+    </table> <br>  
+</body> 
+</html>
--- index.php
+++ index.php
@@ -1,9 +1,19 @@
+<?php
+    
+    $_SESSION['params'] = array();
+?>
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8"> 
     <title> Вариант 28710</title>
     <style type="text/css">
+        header
+        {
+            font-size: 10pt;
+            font-family: Sans-Serif;
+            color: #353535;
+        }
         li 
         {
             font-weight: bold;
@@ -17,8 +27,7 @@
             text-align: center; 
         }
         input 
-        {
-            
+        {           
             padding: 5px 10px; 
             color: #353535; 
             margin-top: 20px;
@@ -29,10 +38,26 @@
             var x = document.forms["params"]["X"].value;
             var y = document.forms["params"]["Y"].value;
             var r = document.forms["params"]["R"].value;
-            if (x == "" ){
-                
+            var isCorrect = true;
+            var errorMessage = "";
+            if (x ==""){
+                isCorrect = false;
+                errorMessage +="Неккоректно задано значение X\n";
             }
-            return true;
+            if (y == "" || y.length > 10 || isNaN(y) || y < -3 || y > 3){
+                errorMessage += "Некорректно задано значение Y\n";
+                isCorrect = false;
+            }
+            if (r == "" || r.length > 10 || isNaN(r) || r < 2 || r > 5){
+                errorMessage += "Некорректно задано значение R";
+                isCorrect = false;
+            }
+            if (isCorrect){
+                return true;
+            } else {
+                alert(errorMessage);
+                return false;
+            }
         }
     </script>
 </head>
@@ -52,17 +77,17 @@
     <form id="params" method="post" action="check.php">
         <div class="input-field">
             <b> X: </b>
-                <?php
-                    for ($i=-3; $i <= 5 ; $i++) { 
-                        echo '<input type="radio" name="X" value="',$i,'">',$i;
-                    }
-                ?> 
+            <?php
+                for ($i=-3; $i <= 5 ; $i++) { 
+                    echo '<input type="radio" name="X" value="',$i,'">',$i;
+                }
+            ?> 
         </div>
         <div class="input-field">   
-            <b> Y: </b> <input type="text" name="Y">     
+            <b> Y: </b> <input type="text" name="Y" placeholder="(-3 .. 3)">     
         </div>      
         <div class="input-field">
-            <b> R: </b> <input type="text" name="R">
+            <b> R: </b> <input type="text" name="R" placeholder="(2 .. 5)">
         </div>
         <div class="input-field" onclick="return validate()">
             <input type="submit" value="Проверить точку"> 
