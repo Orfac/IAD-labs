dc7e1fb8fae80d6aee38bdfbf79dac1302f84cb4
Orfac <molodeckij.b.2017@fml31.ru>
2018-09-10 16:04:38 +0300

    fixed comparesin

--- index.php
+++ index.php
@@ -38,17 +38,19 @@
             var x = document.forms["params"]["X"].value;
             var y = document.forms["params"]["Y"].value;
             var r = document.forms["params"]["R"].value;
+            console.log(x[0]);
+            console.log(y[0]);
             var isCorrect = true;
             var errorMessage = "";
             if (x ==""){
                 isCorrect = false;
                 errorMessage +="Неккоректно задано значение X\n";
             }
-            if (y == "" || y.length > 10 || isNaN(y) || y < -3 || y > 3){
+            if (y == "" || isNaN(y) || y <= "-3" || y >= "3" ){
                 errorMessage += "Некорректно задано значение Y\n";
                 isCorrect = false;
             }
-            if (r == "" || r.length > 10 || isNaN(r) || r < 2 || r > 5){
+            if (r == "" || isNaN(r) || y <= "2"  || r >= "5"){
                 errorMessage += "Некорректно задано значение R";
                 isCorrect = false;
             }
