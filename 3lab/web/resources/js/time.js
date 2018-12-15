178af536a2f64bde4873a300c2cb8d50a21cd5c0
arseniy <molodeckij.b.2017@fml31.ru>
2018-12-16 02:58:10 +0300

    Redesigned date-time in start page

--- 3lab/web/resources/js/time.js
+++ 3lab/web/resources/js/time.js
@@ -1,11 +1,21 @@
+let days = document.getElementById('day');
+let hours = document.getElementById('hours');
+let minutes = document.getElementById('minutes');
+let seconds = document.getElementById('seconds');
+let month = document.getElementById('month');
+let year = document.getElementById('year');
+
+
 function updateClock() {
-    var now = new Date();
-    var time = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
-// a cleaner way than string concatenation
-    var date = [now.getDate(), now.getMonth() + 1,
-        now.getFullYear()].join('.');
-    document.getElementById('time').innerHTML = [time, date].join(' | ');
-    setTimeout(updateClock, 12000);
+    let now = new Date();
+    days.innerHTML = ('0' + now.getDate().toString()).slice(-2);
+    hours.innerHTML = ('0' + now.getHours().toString()).slice(-2);
+    minutes.innerHTML = ('0' + now.getMinutes().toString()).slice(-2);
+    seconds.innerHTML = ('0' + now.getSeconds().toString()).slice(-2);
+    month.innerHTML = ('0' + (now.getMonth()+1).toString()).slice(-2);
+    year.innerHTML = now.getFullYear().toString();
+
+    setTimeout(updateClock, 11000);
 }
 
 updateClock();
